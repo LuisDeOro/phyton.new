e3bcbd1b5de3c3fa73af67105cb9ab1784144a20
LuisDeOro <luisdeoro1005@gmail.com>
2024-04-24 16:58:53 -0500

    python.newww

---
+++ python.py
@@ -0,0 +1,14 @@
+print ("hola mundo")
+socio = 17
+print(socio)
+print(type(socio))
+mas_o_menos = "sou-sou"
+print(mas_o_menos)
+print(type(mas_o_menos))
+cadena1 = "hola"
+cadena2 = "Mundo"
+result = (cadena1+""+cadena2)
+print(result)
+mi_cadena="bienvenidos"
+sud_cadena = mi_cadena[::-1]
+print(sud_cadena)
---
+++ socio.py
@@ -0,0 +1,14 @@
+#problematica escribir un programa que me devuelva el nombre de una persona alreves y con reemplazo de palabras.
+nombre = "luis carlos de oro"
+print(nombre)
+nombre_invertido = nombre[::-1]
+print(nombre_invertido)
+socio1 = "Quiubo"
+socio2 = "ñero"
+resultado =socio1+" "+socio2
+print(resultado)
+longitud =len(nombre)
+print(longitud)
+men = "mayuscula"
+minuscula = men.lower
+print(minuscula)
