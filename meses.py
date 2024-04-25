4abc2302b8987759a7ec8e0f0b0ffd0841375a07
LuisDeOro <luisdeoro1005@gmail.com>
2024-04-25 15:33:36 -0500

    meses del año python

---
+++ meses.py
@@ -0,0 +1,30 @@
+meses = str(input("ingrese el mes que quieres: de (enero, febrero, marzo, abril, abril, mayo, junio, julio, agosto, septiembre, octubre, noviembre, diciembre): "))
+if meses ==("enero"):
+    print("este mes tiene 31 dias")
+elif meses ==("febrero"):
+    print("este mes tiene 29 dias")
+elif meses ==("marzo"):
+    print("este mes tiene 31 dias")
+elif meses ==("abril"):
+    print("este mes tiene 30 dias")
+elif meses ==("mayo"):
+    print("este mes tiene 31 dias")
+elif meses ==("junio"):
+    print("este mes tiene 31 dias")
+elif meses ==("julio"):
+    print("este mes tiene 31 dias")
+elif meses ==("agosto"):
+    print("este mes tiene 31 dias")
+elif meses ==("septiembre"):
+    print("este mes tiene 30 dias")
+elif meses ==("octubre"):
+    print("este mes tiene 31 dias")
+elif meses ==("noviembre"):
+    print("este mes tiene 30 dias")
+elif meses ==("diciembre"):
+    print("este mes tiene 31 dias")
+else:
+    print("ingrese un valor valido")
+print("gracias socio")
+
+    
