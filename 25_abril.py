aa9316f43c4c8c999125b9c9ad882579d0e7ed37
LuisDeOro <luisdeoro1005@gmail.com>
2024-04-25 14:48:01 -0500

    Clase_25_abril_python

---
+++ 25_abril.py
@@ -0,0 +1,11 @@
+#operaciones aritmeticas
+inicio = "bienvenidos"
+nombre = input(" cual es tu nombre: ")
+print("´Hola ",nombre," realizaremos una suma`")
+numero_uno = int(input("escribe el primer numero: "))
+numero_dos = int(input("escribe el segundo numero: "))
+resultado = (numero_uno+numero_dos)
+print("el resultado de la suma es: ",numero_uno,"+",numero_dos,"=",resultado)
+if resultado<23:
+    print("no sirve")
+print("si sirve")   
